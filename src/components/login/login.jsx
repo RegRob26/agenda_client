77dcad93ca0cde8c0637fb388d5e571596bacc56
Emmanuel Robles <rore021226@gs.utm.mx>
2024-06-11 07:49:43 -0600

    feat(login): redirect to singup when user not register yet

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -8,19 +8,22 @@ import {
     FormItem,
     FormLabel,
     FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@/components/ui/button.jsx';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Login(props) {
+    const navigate = useNavigate();
     const schema = z.object({
         email: z.string().email({
-            message: 'Ingresa un correo válido'
+            message: 'Ingresa un correo válido',
         }),
         password: z.string().min(6, {
-            message: 'La contraseña debe tener al menos 8 caracteres'
+            message: 'La contraseña debe tener al menos 8 caracteres',
         }),
     });
 
@@ -30,7 +33,7 @@ function Login(props) {
             email: '',
             password: '',
         },
-    })
+    });
 
 
     function onSubmit(event) {
@@ -39,13 +42,13 @@ function Login(props) {
 
     return (
         <div>
-            <div className='flex flex-col md:flex-row'>
-                <div className='md:w-1/2'>
+            <div className="flex flex-col md:flex-row">
+                <div className="md:w-1/2">
                     <img src="/4991945.jpg"
-                         className='w-full hidden md:block'
+                         className="w-full hidden md:block"
                          alt="Imagen grande" />
                 </div>
-                <div className='md:w-1/2 py-14'>
+                <div className="md:w-1/2 py-14">
                     <div>
                         <img
                             className="mx-auto h-12 w-auto"
@@ -53,56 +56,55 @@ function Login(props) {
                             alt="Your Company"
                         />
                         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                            Sign in to your account
+                            Ingresa a tu cuenta
                         </h2>
                     </div>
                     <div className="mt-10 px-8 md:mx-auto md:max-w-sm">
                         <Form {...form}>
-                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                                <div>
-                                    <FormField
-                                        control={form.control}
-                                        name="email"
-                                        render={({ field }) => (
-                                            <div>
-                                                <FormItem>
-                                                    <FormLabel>Correo</FormLabel>
-                                                    <FormControl>
-                                                        <Input placeholder={'correo'}  {...field} autoComplete="email"
-                                                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <div>
+                                            <FormItem>
+                                                <FormLabel>Correo</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder={'correo'}  {...field} autoComplete="email"
+                                                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
                                    ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600
                                    sm:text-sm sm:leading-6" />
-                                                    </FormControl>
-                                                    <FormMessage />
-                                                </FormItem>
-                                            </div>
-                                        )}
-                                    />
-                                    <FormField
-                                        control={form.control}
-                                        name="password"
-                                        render={({ field }) => (
-                                            <div>
-                                                <FormItem>
-                                                    <FormLabel>Contraseña</FormLabel>
-                                                    <FormControl>
-                                                        <Input type="password" placeholder={'contraseña'}  {...field}
-                                                               autoComplete="password"
-                                                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        </div>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <div>
+                                            <FormItem>
+                                                <FormLabel>Contraseña</FormLabel>
+                                                <FormControl>
+                                                    <Input type="password" placeholder={'contraseña'}  {...field}
+                                                           autoComplete="password"
+                                                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset
                                       ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600
                                         sm:text-sm sm:leading-6" />
-                                                    </FormControl>
-                                                    <FormMessage />
-                                                </FormItem>
-                                            </div>
-                                        )}
-                                    />
-                                </div>
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        </div>
+                                    )}
+                                />
                                 <div>
                                     <Button
                                         type="submit"
-                                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                    >
+                                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold
+                                        leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
+                                        focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                                         Ingresar
                                     </Button>
                                 </div>
@@ -111,9 +113,10 @@ function Login(props) {
 
                         <p className="mt-10 text-center text-sm text-gray-500">
                             ¿Aún no tienes una cuenta? {' '}
-                            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                            <Link to="/signup"
+                                  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
                                 Prueba la app
-                            </a>
+                            </Link>
                         </p>
                     </div>
 
