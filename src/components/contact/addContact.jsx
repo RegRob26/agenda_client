d2f435480b07aa86db3c3d150ea6213fd2353165
Emmanuel Robles <rore021226@gs.utm.mx>
2024-06-14 12:18:36 -0600

    fix(addcontact): fixed bad validation

--- src/components/contact/addContact.jsx
+++ src/components/contact/addContact.jsx
@@ -65,11 +65,11 @@ function AddContact(props) {
             message: 'El apellido debe tener como máximo 30 caracteres'
         }),
 
-        second_last_name: z.string().min(2, {
-            message: 'El nombre debe tener al menos 2 caracteres'
+        second_last_name:z.string().min(2, {
+            message: 'El segundo nombre debe tener al menos 2 caracteres'
         }).max(30, {
-            message: 'El nombre debe tener como máximo 30 caracteres'
-        }),
+            message: 'El segundo nombre debe tener como máximo 30 caracteres'
+        }).or(z.string().max(0)),
         phone: z.string().min(10, {
             message: 'El teléfono debe tener al menos 10 caracteres'
         }).max(15, {
